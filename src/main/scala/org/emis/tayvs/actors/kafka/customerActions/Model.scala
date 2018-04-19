b7d1ea1b46a6bce8f3a7b6ea4bdda546a16c6bb7
sstolyarenko <svyatoslav.stolyarenko@webtech.in.ua>
2018-04-19 15:02:47 +0300

    - Add Customer Action object

--- src/main/scala/org/emis/tayvs/actors/kafka/customerActions/Model.scala
+++ src/main/scala/org/emis/tayvs/actors/kafka/customerActions/Model.scala
@@ -1,9 +1,28 @@
 package org.emis.tayvs.actors.kafka.customerActions
 
+import reactivemongo.bson.BSONDateTime
+
 object Model {
   
   case class Click(link: String, customer: String = "avatar", time: Long = System.currentTimeMillis())
   case class ShowImg(link: String, customer: String = "avatar", time: Long = System.currentTimeMillis())
   case class DownloadFile(fileSource: String, customer: String = "avatar", time: Long = System.currentTimeMillis())
+  
+  case class CustomerAction(
+    id: Option[String] = None,
+    emailId: String,
+    campaignId: String,
+    creativeId: String,
+    offerId: String,
+    subjectId: String,
+    fromNameId: String,
+    domain: String,
+    remoteAdr: String,
+    action: String,
+    activeKey: String,
+    serverId: String,
+    dateTime: BSONDateTime,
+    headers: Seq[String]
+  )
   
 }
